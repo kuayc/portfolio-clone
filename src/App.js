1db0900571f788002eb952e2c48b4bf52d474bee
Chin Kuay <kuaychin@gmail.com>
2020-04-13 14:01:52 -0500

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>HELLO DFW</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  setScrollHeight = (value) => {
+    window.scrollTo(+value, +value);
+    console.log(value);
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="nav-container">
+          <nav className="nav">
+            <span className="title">Bootcamp</span>
+            <p class="nav-link js-scroll-trigger" className="nav-links">
+              SERVICES
+            </p>
+            <p className="nav-links">PORTFOLIO</p>
+            <p className="nav-links">ABOUT</p>
+            <p className="nav-links">TEAM</p>
+            <p className="nav-links">CONTACT</p>
+            <button className="menu-bars">menu</button>
+          </nav>
+
+          <div className="welcome-container">
+            <p className="welcome1">Welcome to DFW!</p>
+            <p className="welcome2">Nice to meet you</p>
+            <button
+              value="800"
+              onClick={() => this.setScrollHeight()}
+              className="tell-more"
+            >
+              Tell Me More
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
